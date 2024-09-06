a8b8a2b199d28ba6dcabd3b50c4786a5109ed483
ghimireaacs <aacs.csu@gmail.com>
2024-09-06 22:18:35 +1000

    Updated Workflow to main

--- tools/release.sh
+++ tools/release.sh
@@ -1,29 +1,22 @@
 #!/usr/bin/env bash
 #
-# Build, test and then deploy the site content to 'origin/<pages_branch>'
+# Build, test, and deploy the site content to 'origin/<pages_branch>'
 #
-# Requirement: html-proofer, jekyll
+# Requirement: Jekyll, Bundler
 #
 # Usage: See help information
 
 set -eu
 
 PAGES_BRANCH="gh-pages"
-
 SITE_DIR="_site"
-
 _opt_dry_run=false
-
 _config="_config.yml"
-
-_no_pages_branch=false
-
-_backup_dir="$(mktemp -d)"
-
 _baseurl=""
+_commit_message="[Automation] Site update No.${GITHUB_RUN_NUMBER:-0}" # GITHUB_RUN_NUMBER for incremental commits
 
 help() {
-  echo "Build, test and then deploy the site content to 'origin/<pages_branch>'"
+  echo "Build, test, and deploy the site content to 'origin/<pages_branch>'"
   echo
   echo "Usage:"
   echo
@@ -37,123 +30,99 @@ help() {
 
 init() {
   if [[ -z ${GITHUB_ACTION+x} && $_opt_dry_run == 'false' ]]; then
-    echo "ERROR: It is not allowed to deploy outside of the GitHub Action envrionment."
-    echo "Type option '-h' to see the help information."
-    exit -1
+    echo "ERROR: It is not allowed to deploy outside of the GitHub Action environment."
+    echo "Use '-h' for help."
+    exit 1
   fi
 
   _baseurl="$(grep '^baseurl:' _config.yml | sed "s/.*: *//;s/['\"]//g;s/#.*//")"
 }
 
 build() {
-  # clean up
-  if [[ -d $SITE_DIR ]]; then
-    rm -rf "$SITE_DIR"
-  fi
+  # Clean up previous builds
+  rm -rf "$SITE_DIR"
 
-  # build
-  JEKYLL_ENV=production bundle exec jekyll b -d "$SITE_DIR$_baseurl" --config "$_config"
+  # Build with Jekyll
+  JEKYLL_ENV=production bundle exec jekyll build -d "$SITE_DIR$_baseurl" --config "$_config"
 }
 
+# Optional test function (currently disabled)
 test() {
-  bundle exec htmlproofer \
-    --disable-external \
-    --check-html \
-    --allow_hash_href \
-    "$SITE_DIR"
+  bundle exec htmlproofer --disable-external --check-html --allow_hash_href "$SITE_DIR"
 }
 
-resume_site_dir() {
+prepare_site_dir() {
   if [[ -n $_baseurl ]]; then
-    # Move the site file to the regular directory '_site'
     mv "$SITE_DIR$_baseurl" "${SITE_DIR}-rename"
     rm -rf "$SITE_DIR"
     mv "${SITE_DIR}-rename" "$SITE_DIR"
   fi
 }
 
-setup_gh() {
-  if [[ -z $(git branch -av | grep "$PAGES_BRANCH") ]]; then
-    _no_pages_branch=true
-    git checkout -b "$PAGES_BRANCH"
-  else
+setup_gh_pages_branch() {
+  if git show-ref --quiet refs/heads/$PAGES_BRANCH; then
     git checkout "$PAGES_BRANCH"
+  else
+    git checkout --orphan "$PAGES_BRANCH"
   fi
-}
-
-backup() {
-  mv "$SITE_DIR"/* "$_backup_dir"
-  mv .git "$_backup_dir"
-
-  # When adding custom domain from Github website,
-  # the CANME only exist on `gh-pages` branch
-  if [[ -f CNAME ]]; then
-    mv CNAME "$_backup_dir"
-  fi
-}
-
-flush() {
-  rm -rf ./*
-  rm -rf .[^.] .??*
-
-  shopt -s dotglob nullglob
-  mv "$_backup_dir"/* .
-  [[ -f ".nojekyll" ]] || echo "" >".nojekyll"
 }
 
 deploy() {
   git config --global user.name "GitHub Actions"
   git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 
-  git update-ref -d HEAD
-  git add -A
-  git commit -m "[Automation] Site update No.${GITHUB_RUN_NUMBER}"
+  # Remove all files from the current directory (in the gh-pages branch)
+  git rm -rf .
 
-  if $_no_pages_branch; then
-    git push -u origin "$PAGES_BRANCH"
-  else
-    git push -f
-  fi
+  # Add the generated site files
+  cp -r "$SITE_DIR"/* .
+
+  # Create or update .nojekyll to bypass Jekyll on GitHub Pages
+  [[ -f ".nojekyll" ]] || echo "" >".nojekyll"
+
+  # Commit and push changes to the gh-pages branch
+  git add -A
+  git commit -m "$_commit_message"
+  git push -f origin "$PAGES_BRANCH"
 }
 
 main() {
   init
   build
-  # test
-  resume_site_dir
+  # test  # Uncomment this line if you want to enable HTML-Proofer testing
+  prepare_site_dir
 
   if $_opt_dry_run; then
+    echo "Dry run completed. No deployment was made."
     exit 0
   fi
 
-  setup_gh
-  backup
-  flush
+  setup_gh_pages_branch
   deploy
 }
 
+# Parse command-line arguments
 while (($#)); do
   opt="$1"
   case $opt in
-  -c | --config)
-    _config="$2"
-    shift
-    shift
-    ;;
-  --dry-run)
-    # build & test, but not deploy
-    _opt_dry_run=true
-    shift
-    ;;
-  -h | --help)
-    help
-    exit 0
-    ;;
-  *)
-    # unknown option
-    help
-    exit 1
-    ;;
+    -c | --config)
+      _config="$2"
+      shift
+      shift
+      ;;
+    --dry-run)
+      _opt_dry_run=true
+      shift
+      ;;
+    -h | --help)
+      help
+      exit 0
+      ;;
+    *)
+      # Unknown option
+      help
+      exit 1
+      ;;
   esac
 done
 
